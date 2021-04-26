debdf02dd2223b59163ee7db5fecc139b029f5f7
DC <473159726@qq.com>
2021-04-26 10:15:05 +0800

    Initial commit

---
+++ src/main/java/com/dc/java/BaseJavaStudyLanguage.java
@@ -0,0 +1,23 @@
+package com.dc.java;
+
+/**
+ * @author: 一块儿小饼干
+ * @ProjectName: StudyRecord
+ * @Description: 基础java学习
+ * @date: 2021/4/26 10:06
+ */
+public class BaseJavaStudyLanguage {
+
+    /**
+     * 常见的数据类型及其对应的封装类
+     */
+    public void baseClass(){
+        //int->Integer
+        //char->Character
+        //long->Long
+        //bool->Boolean
+        //double->Double
+        //short->Short
+        //byte->Byte
+    }
+}
