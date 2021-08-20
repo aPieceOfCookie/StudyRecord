a4905b3d84f624b7f425a3fb7f01b68dfd718e4d
dc <473159726@qq.com>
2021-08-20 17:09:36 +0800

    处理面试问题

---
+++ src/main/java/com/meeting/JavaInformation.java
@@ -0,0 +1,18 @@
+package com.meeting;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class JavaInformation {
+
+    /**
+     * HashMap、LinkedHashMap、ConcurrentHashMap、ArrayList、LinkedList的底层实现。
+     * hashMap: 底层是map
+     */
+    public void collInfo(){
+        Map<String,String> map=new HashMap<>();
+        Map<String,String> lMap=new LinkedHashMap<>();
+    }
+
+}
