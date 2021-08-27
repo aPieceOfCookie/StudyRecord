14f09b6e8eb826f19bb30b99221963f63989844a
dc <473159726@qq.com>
2021-08-27 18:05:28 +0800

    处理面试问题

--- src/main/java/com/meeting/JavaInformation.java
+++ src/main/java/com/meeting/JavaInformation.java
@@ -1,10 +1,14 @@
 package com.meeting;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 public class JavaInformation {
+
 
     /**
      * HashMap、LinkedHashMap、ConcurrentHashMap、ArrayList、LinkedList的底层实现。
@@ -13,6 +17,24 @@ public class JavaInformation {
     public void collInfo(){
         Map<String,String> map=new HashMap<>();
         Map<String,String> lMap=new LinkedHashMap<>();
+        Collections.synchronizedMap(map);
+    }
+
+    /**
+     * 实现callable接口，重写call方法<br>
+     * 此方法是有返回值的
+     * 并发编程
+     */
+    public void callableFn(){
+        class Demo implements Callable<Integer>{
+
+            @Override
+            public Integer call() throws Exception {
+                return null;
+            }
+        }
+        Demo demo = new Demo();
+        FutureTask<Integer> task = new FutureTask<Integer>(demo);
     }
 
 }
